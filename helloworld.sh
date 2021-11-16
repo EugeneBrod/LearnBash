44740b377a060cdf064e36545234e4ee9e406875
EugeneBrod <eugenebrod@gmail.com>
2021-11-15 21:09:44 -0800

    whileloop

--- helloworld.sh
+++ helloworld.sh
@@ -1,1 +1,11 @@
 echo "Hello World!"
+echo -n "printint without newline"
+echo -e "\nRemoving \t backslash \t characters \n"
+echo "\nNot \t removing \t backslashes \n"
+
+#this is a comment, notice the hash symbol
+
+: '
+The following script calculates
+the square value of the number, 5.
+'
